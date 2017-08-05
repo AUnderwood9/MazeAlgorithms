02970867ffc0f8609cb32f9655a072a9fc9cd56e
AUnderwood9 <aunderwood797@gmail.com>
2017-08-05 15:14:16 -0500

    Moved all of the initial user input into a method for modularity and readability

--- CSharp/Mazes/Mazes/Program.cs
+++ CSharp/Mazes/Mazes/Program.cs
@@ -126,6 +126,8 @@ namespace Mazes
                     tool.DrawLine(blackPen, xOfUpperLeft, yOfLowerRight, xOfLowerRight, yOfLowerRight);
                 }
 
+                //Use Draw text here to draw the distance of the curent room.
+                //tool.DrawString();
 
                 double intensity = ((double)maxDistance - (double)room.distance) / ((double)maxDistance);
                 double dark = Math.Round((255d * intensity));
